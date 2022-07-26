5646ee12deec5d40013e887fc9ccf7519025ea64
Anton <94136841+kodareAnton@users.noreply.github.com>
2022-07-26 20:12:26 +0200

    almost finnish

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import axios from 'axios';
 import { request, ServerResponse } from 'http';
 import { Http2ServerRequest } from 'http2';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
 
@@ -15,7 +16,6 @@ function App() {
   
   const [newUser, setNewUser] = useState<InewUser>({username:'', password:'', subscribe:false})
   const [logginUser, setLoggin] = useState<InewUser>({username:'', password:'', subscribe:false})
-  
   
   // pushar till backend -> api
   function sendToApi(){
@@ -31,7 +31,7 @@ function App() {
 function handleChange(e:ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>){
   let name:string = e.target.name;
 
-  if(name == 'username' || name == 'password'){ 
+  if(name == 'username' || name == 'password'){
     setNewUser({...newUser, [name]: e.target.value});
 }
   else{
@@ -52,13 +52,18 @@ interface Iusers {
   const [error, setError] = useState(null);
   const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  
+  const navigate = useNavigate();
 
-  const handleLoggin = () =>{
+  const handleLoggin = (e: any) =>{
+    e.preventDefault();
     axios.post('http://localhost:3000/users/loggin', {
       username: username,
       password: password
     }).then(response => {
-      console.log('response', response);
+      navigate('/loggedin')
+      let GetPostData = response.data
+      return localStorage.setItem('_id', GetPostData._id)
     }).catch(error => {
       console.log('err', error);
     })
@@ -68,19 +73,17 @@ interface Iusers {
     <div className="App">
 
     <h1>Loggin</h1>
-
     <form>
       <label htmlFor="username">username</label><br />
       <input type="text" value={username} onChange={e => setUserName(e.target.value)} /><br />
       <label htmlFor="password">password</label><br />
       <input type="text" value={password} onChange={e => setPassword(e.target.value)} /><br />
-      <button onClick={handleLoggin}>skicka</button><br />
+      <button onClick={handleLoggin}>Loggin</button><br />
 
       {error && <p className='error'>{error}</p>}
     </form>
 
 <h1>Create User</h1>
-
     <form>
       <label htmlFor="username">user</label><br />
       <input type="text" name='username' onChange={handleChange}
@@ -93,7 +96,6 @@ interface Iusers {
       <button onClick={sendToApi}>skapa</button>
       </form>
     </div>
-    
   );
 }
 
--- src/components/loggedin.tsx
+++ src/components/loggedin.tsx
@@ -1,17 +1,61 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function LoggedIn() {
 
+  interface IUser {
+  _id: '',
+  username: '',
+  password: '',
+  subscribe: false
+  }
+ 
   const [checked, setChecked] = useState(false)
+  const [Data, setData] = useState<IUser>()
   console.log(checked);
-  
+
+  const LocalStorageKey = localStorage.getItem('_id')
+
+  // Hämtar användare med hjälp av nyckel
+  useEffect(() => {
+  axios.post<IUser>('http://localhost:3000/users/user', {
+    _id: LocalStorageKey
+  }).then(response => {
+    let GetPostData = response.data
+    setData(GetPostData)
+  }).catch(error => {
+    console.log('err', error);
+  })
+}, [])
+
+// Ändrar användarens innehåll med hjälp av nyckel
+function savePost(){
+  axios
+      .put("http://localhost:3000/users/"+LocalStorageKey, {
+        _id: LocalStorageKey,
+        subscribe: checked 
+      }, {
+        headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    })
+      .then((res) => {
+        console.log('result',res);
+      })
+      .catch((err) => {
+        console.log(err, "Inte skickat");
+      });
+  }
 
   return (
     <>
-      <h1>HEJ!</h1>
+      <h1>Hej {Data?.username}!</h1>
+      <p>klicka i kryssboxen för att prenumerera eller att avprenumerera!!</p>
       <input type="checkbox" 
       defaultChecked={checked}
       onChange={() => setChecked(!checked)}/>
+
+      <p>Prenumerationsstatus: {String(Data?.subscribe)}</p>
+      <br />
+      <button onClick={savePost}>Skicka in</button>
     </>
   );
 }
